dc0335d022c1c171f492e2681676d4924c98fb74
mfln <zydziupolska@gmail.com>
2026-07-17 10:51:44 +0200

    consistency with Dynomic.pro

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,13 +45,9 @@ android {
 
     buildTypes {
         release {
-            signingConfig    = signingConfigs.getByName("release")
-            isMinifyEnabled  = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            signingConfig     = signingConfigs.getByName("release")
+            isMinifyEnabled   = false
+            isShrinkResources = false
         }
     }
 }
